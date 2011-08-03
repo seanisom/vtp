62e91fe8c6087cd2cf6ba202379dbaf3cc376219
roger@beardandsandals.co.uk <roger@beardandsandals.co.uk@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2011-08-03 09:19:27 +0000

    Kill redundant branch

--- branches/rogers-sandbox/TerrainApps/Enviro/vtlib_headers.cpp
+++
@@ -1,7 +0,0 @@
-//
-// This file exists to build the precompiled headers for the modules
-//  which utilize vtlib.
-//
-
-#include "vtlib/vtlib.h"
-
--- branches/rogers-sandbox/TerrainSDK/vtlib/pre.cpp
+++
@@ -1,5 +0,0 @@
-//
-// This file helps MSVC to build the precompiled headers, once.
-// It serves no purpose in other build environments.
-//
-#include "vtlib/vtlib.h"
