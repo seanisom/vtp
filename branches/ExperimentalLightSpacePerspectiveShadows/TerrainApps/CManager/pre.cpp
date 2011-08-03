417e4bc8dfd0de29adbc1fbb5c2da0b6240803ef
roger@beardandsandals.co.uk <roger@beardandsandals.co.uk@2ba57974-ef2e-0410-970c-f3cb55f6a86a>
2011-08-03 09:18:23 +0000

    Kill redundant branch

--- branches/ExperimentalLightSpacePerspectiveShadows/TerrainApps/CManager/pre.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Name:     pre.cpp
-// Purpose:  Precompiled header module.  Only used for MSVC.
-//
-
-// For compilers that support precompilation, includes "wx.h".
-#include "wx/wxprec.h"
--- branches/ExperimentalLightSpacePerspectiveShadows/TerrainApps/glutSimple/Data/Elevation/README.txt
+++
@@ -1,5 +0,0 @@
-
-By default, wxSimple will look for the file "crater_0513.bt"
-in this directory.  You should copy the file here from the
-folder Enviro/Data/Elevation.
-
